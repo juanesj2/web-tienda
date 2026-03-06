3c524b0332d07ee0f9d76cc27daa9db98d90c294
juanesj2 <juanstivenaa82@gmail.com>
2026-03-07 00:15:27 +0100

    fix: trust all proxies for HTTPS detection on Render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Trust all proxies (needed for Render.com SSL termination)
+        $middleware->trustProxies(at: '*');
+
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
         ]);
